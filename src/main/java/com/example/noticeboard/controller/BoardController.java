ca81b4dd2924bd349ccf1a7c2cb692dd419c5b9a
seungwook <rlatmd0829@naver.com>
2021-04-01 21:36:44 +0900

    페이지 구현

--- src/main/java/com/example/noticeboard/controller/BoardController.java
+++ src/main/java/com/example/noticeboard/controller/BoardController.java
@@ -11,6 +11,10 @@ import com.example.noticeboard.security.UserDetailsImpl;
 import com.example.noticeboard.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -53,16 +57,24 @@ public class BoardController {
 //    }
 
     @GetMapping("/")
-    public String getIndex(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails){
-        List<Board> board1 = boardRepository.findAllByOrderByModifiedAtDesc();
-        System.out.println(userDetails);
+    public String getIndex(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails,@PageableDefault(size=5) Pageable pageable){
+        Page<Board> board = boardRepository.findAllByOrderByModifiedAtDesc(pageable);
+        //board.getTotalElements(); // 전체데이터 건수
+
         if(userDetails == null){
             model.addAttribute("user","null");
         }else{
 
             model.addAttribute("user",userDetails.getUser().getUsername());
         }
-        model.addAttribute("board",board1);
+        // 현재 페이지 넘버 - 4 (4는 임의로 정한값)을 뺀값을 보여줄 페이지 값에 첫번째 값으로 지정
+        // 현재 페이지 넘버 + 4 을 더한값을 보여줄 페이지에서 끝값으로 표시
+        int startPage = Math.max(1, board.getPageable().getPageNumber() - 4); //그런데 음수가 나올수 있으니 max() 함수를 이용해 0보다 작은값은 나오지 않도록 만들어줌
+        int endPage = Math.min(board.getTotalPages(),board.getPageable().getPageNumber() + 4); // 마찬가지로 최대페이지수를 초과하지않게 min()함수 걸어줌
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("board",board);
         return "index";
     }
 
--- src/main/java/com/example/noticeboard/repository/BoardRepository.java
+++ src/main/java/com/example/noticeboard/repository/BoardRepository.java
@@ -2,10 +2,14 @@ package com.example.noticeboard.repository;
 
 import com.example.noticeboard.models.Board;
 import com.example.noticeboard.models.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board,Long> {
-    List<Board> findAllByOrderByModifiedAtDesc();
+
+    Page<Board> findAllByOrderByModifiedAtDesc(Pageable pageable);
 }
